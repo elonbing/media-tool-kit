fb50ffa57bbb2bfdcdde295bfb656844b536c22f
Luis Queral <lqueral@blenderbox.com>
2017-05-01 18:10:37 -0400

    adding untracked files

---
+++ mtk-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+###############################################################################
+# Media Tool Kit Installer
+# --------------------------------
+# This script installs all of the requirements for the Media Tool Kit framework
+# and it's collection of various scripts and utilities.
+#
+# By default, this script will install the scripts code in the current user's
+# home directory and all of its dependencies (including libraries and database
+# servers) at the system level. You may of course choose to install this within
+# a virtual environment.
+#
+# As new scripts are added, we recommend adding installation requirements here!
+#
+###############################################################################
+
+# Name of Script
+* command to install dependencies *
+
+# Name of Script
+* command to install dependencies *
+
+# Name of Script
+* command to install dependencies *
+
+etc....
