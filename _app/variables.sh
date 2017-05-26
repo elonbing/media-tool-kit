4722816ce6a5d59cd5fb8acd301dcd84f6db9407
Luis Queral <lqueral@blenderbox.com>
2017-05-25 21:35:06 -0400

    variables.sh work, finally figured out some bash best pracitces

--- _app/variables.sh
+++ _app/variables.sh
@@ -1,40 +1,35 @@
-#!/bin/bash
-# These are some useful commands to pass as values in your controller script!
+#!/usr/bin/env bash
+
+# Boilerplate Shite
+# //////////////////////////////
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
+
+arg1="${1:-}"
+
+# Control Script Tools
 # //////////////////////////////
 
 # Select Random File in Directory
+RANDOM_FILE_IN_DIR="$(ls | head -$((RANDOM%$(ls | wc -w)+1)) | tail -1)"
 
 # Select Most Recent File in Directory
-RECENT_FILE_IN_DIR=ls -Art | tail -n 1
+RECENT_FILE_IN_DIR="$(ls -t | head -n1)"
 
-# Generate Random Number
-RANDOM_NUM=echo $((1 + RANDOM % 10))
+# Random Integer
+RANDOM_NUM="$((1 + RANDOM % 9))"
 
-# ascii
+# Test Varaibles Within Variables
+#RANDOM_NUM="$((1 + RANDOM % 9))"
+#TEST="I'm a Varible in a String! $RANDOM_NUM"
+#echo "I'm a $TEST another $RANDOM_NUM"
 
-hello=cat << "EOF"
-
-                 .-""-.
-                /      \
-               /     (0 \______
-               |         "_____)
-               \        ,-----'
-                \_    _/
-                 /    \
-                /      \
-               /        \
-              /          |
-             /        :  |
-            /     ;   :  |
-   \\\     /  _.-'    :  |
-    \\\\  / _'        :  |
-     \\\\/ ;         :   /
-      \\  ;         :   /
-       \   `._`-'_.'  _/
-        \     ''' _.-'
-         \      / /
-          \    / /
-           \  /)(_______
-            )(_________<
-           (__________<
-EOF
+echo "${RANDOM_FILE_IN_DIR}"
